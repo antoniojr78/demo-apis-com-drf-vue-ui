1c31b71a6408582ed264a6dad3b2d9f4ef284db8
antoniojr <antonio.silva@wikki.com.br>
2020-01-31 11:12:38 -0300

    Listing Authors fetched via API.

--- my_project-ui/src/components/Authors.vue
+++ my_project-ui/src/components/Authors.vue
@@ -1,12 +1,21 @@
 <template>
   <!-- Obs.: Div required -->
   <div>
-      {{ authors }}
+      <h1 class="text-center text-info">List of Authors</h1>
+      <ul class="list-group-item" v-for="author in authors" v-bind:key="author.id">
+        <li class="list-group-item">{{ author.name }}</li>
+        <li class="list-group-item">{{ author.address }}</li>
+        <li class="list-group-item" v-if="author.site === null || author.site.trim() == ''">Not Informe</li>
+        <li class="list-group-item" v-else>{{ author.site }}</li>
+        <li class="list-group-item">{{ author.email }}</li>
+        <br/>
+      </ul>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
 export default {
   name: 'Authors',
   data(){
