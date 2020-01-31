b015969233fa37f49981edfaf6e36f6d5ff8a742
antoniojr <antonio.silva@wikki.com.br>
2020-01-31 10:50:01 -0300

    Request for API DRF with Axios.

--- my_project-ui/src/App.vue
+++ my_project-ui/src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Authors />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Authors from './components/Authors.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Authors
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ my_project-ui/src/components/Authors.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- Obs.: Div required -->
+  <div>
+      {{ authors }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Authors',
+  data(){
+      return {
+          authors: null
+      }
+  },
+  mounted() {
+      axios.get("http://localhost:8000/api/authors/")
+      .then(resp=>this.authors = resp.data)
+  }
+}
+</script>
